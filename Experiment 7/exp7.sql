d40c1cb93cdff40948811fb274571de22a11ed87
Rohit Kumar <rohitmehta0720@gmail.com>
2025-11-06 23:29:44 +0530

    Create exp7.sql

---
+++ Experiment 7/exp7.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION fn_student_audit()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS 
+$$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        RAISE NOTICE 'Inserted Row -> ID: %, Name: %, Age: %, Class: %',
+                     NEW.id, NEW.name, NEW.age, NEW.class;
+        RETURN NEW;
+
+    ELSIF 
+		TG_OP = 'DELETE' THEN
+        RAISE NOTICE 'Deleted Row -> ID: %, Name: %, Age: %, Class: %',
+                     OLD.id, OLD.name, OLD.age, OLD.class;
+        RETURN OLD;
+    END IF;
+
+    RETURN NULL;
+END;
+$$ 
